d777bae3799a1424be9a10e4a10880036d616b44
Made Wahyu Adwitya Pramana <91488883+wahpram@users.noreply.github.com>
2023-12-11 04:40:44 +0800

    Update recommender_system.py

--- recommender_system.py
+++ recommender_system.py
@@ -1,135 +1,145 @@
 import math
-
 import numpy as np
 import scipy
 import pandas as pd
 import sklearn
 import stopwords
 import tensorflow as tf
+import tensorflow_recommenders as tfrs
+from typing import Dict, Text
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('../data/shared_articles.csv/shared_articles.csv')
-df = df[df['eventType'] == 'CONTENT SHARED']
+df = pd.read_csv('../data/shared_articles.csv/shared_articles.csv').drop(['timestamp',
+                                                                          'authorSessionId',
+                                                                          'authorUserAgent',
+                                                                          'authorRegion',
+                                                                          'authorCountry',
+                                                                          'url'], axis=1)
+interact_df = pd.read_csv('../data/users_interactions.csv/users_interactions.csv').drop(['timestamp',
+                                                                                         'sessionId',
+                                                                                         'userAgent',
+                                                                                         'userRegion'], axis=1)
+print(df.head(0))
 
-interact_df = pd.read_csv('../data/users_interactions.csv/users_interactions.csv')
+# interact_df.dropna(subset=['userCountry'], inplace=True)
+interact_df = interact_df.drop(interact_df[interact_df['eventType'] == 'FOLLOW'].index)
+interact_df = interact_df.drop(interact_df[interact_df['eventType'] == 'BOOKMARK'].index)
+
+df['contentId'] = df['contentId']
 
 event_type = {
     'VIEW': 1.0,
     'LIKE': 2.0,
     'COMMENT CREATED': 4.0,
-    'BOOKMARK': 2.5,
-    'FOLLOW': 3.0,
 }
 
 interact_df['eventStrength'] = interact_df['eventType'].apply(lambda x: event_type[x])
-
-user_interact_count_df = interact_df.groupby(['personId', 'contentId']).size().groupby('personId').size()
-print('users: ', len(user_interact_count_df))
-
-users_req_interact_df = user_interact_count_df[user_interact_count_df >= 4].reset_index()[['personId']]
-print('users with at least 4 interactions: ', len(users_req_interact_df))
-
-print('interactions: ', len(interact_df))
-
-interact_selected_users_df = interact_df.merge(users_req_interact_df, how='right',
-                                               left_on='personId',
-                                               right_on='personId')
-
-print('interactions from users with at least 4 interactions: ', len(interact_selected_users_df))
 
 
 def smooth_preference(x):
     return math.log(1 + x, 2)
 
+content_df = df[['contentId', 'title']]
+print(content_df.head(0))
+print(interact_df.head(0))
 
-interact_full_df = (interact_selected_users_df.
-                    groupby(['personId', 'contentId'])['eventStrength']
-                    .sum().apply(smooth_preference).reset_index())
-print('unique user/item interactions: ', len(interact_full_df))
-print(interact_full_df.head(10))
+interact_df['personId'] = interact_df['personId'].astype(str)
+interactions_df = pd.merge(interact_df, df[['title', 'contentType', 'contentId']], on='contentId', how='left')
 
-content_encoder = LabelEncoder()
-person_encoder = LabelEncoder()
+tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+content_features = tfidf_vectorizer.fit_transform(content_df['title']).toarray()
 
-interact_full_df['encoded_contentId'] = content_encoder.fit_transform(interact_full_df['contentId'])
-interact_full_df['encoded_personId'] = person_encoder.fit_transform(interact_full_df['personId'])
+user_ids = interact_df['personId'].unique()
+content_ids = interact_df['contentId'].unique()
 
-interact_train_df, interact_test_df = train_test_split(interact_full_df,
-                                                       stratify=interact_full_df['personId'],
-                                                       test_size=0.2,
-                                                       random_state=33)
-print('Train dataset: ', len(interact_train_df))
-print('Test dataset: ', len(interact_test_df))
+user_id_to_index = {user_id: index for index, user_id in enumerate(user_ids)}
+content_id_to_index = {content_id: index for index, content_id in enumerate(content_ids)}
 
-def build_content_model():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Input(shape=(1,), name='content_input'),
-        tf.keras.layers.Embedding(input_dim=len(content_encoder.classes_), output_dim=100, input_length=1),
-        tf.keras.layers.Flatten(),  # Flatten layer to connect with Dense layer
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.5),  # Dropout for regularization
-        tf.keras.layers.Dense(1, activation='linear')
-    ])
+interact_df['userIndex'] = interact_df['personId'].map(user_id_to_index)
+interact_df['contentIndex'] = interact_df['contentId'].map(content_id_to_index)
 
-    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=0.1, decay_steps=10000,
-                                   decay_rate=0.9)
-    optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+train_df, test_df = train_test_split(interact_df,
+                                     test_size=0.2,
+                                     random_state=42)
 
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
-                  loss='mse',
-                  metrics=['accuracy'])
+class ContentBasedModel(tf.keras.Model):
+    def __init__(self, num_users, num_content, embedding_dim=50):
+        super(ContentBasedModel, self).__init__()
+        self.user_embedding = tf.keras.layers.Embedding(num_users, embedding_dim, input_length=1)
+        self.content_embedding = tf.keras.layers.Embedding(num_content, embedding_dim, input_length=1)
+        self.flatten = tf.keras.layers.Flatten()
+        self.dot_product = tf.keras.layers.Dot(axes=1)
 
-    return model
+    def call(self, inputs):
+        user_embedding = self.flatten(self.user_embedding(inputs['user']))
+        content_embedding = self.flatten(self.content_embedding(inputs['content']))
+        dot_product = self.dot_product([user_embedding, content_embedding])
+        return dot_product
 
 
-content_model = build_content_model()
+num_users = len(user_ids)
+num_content = len(content_ids)
+model = ContentBasedModel(num_users, num_content)
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics='accuracy')
 
-history = content_model.fit(
-    x=interact_train_df['encoded_contentId'].values,
-    y=interact_train_df['eventStrength'].values,
-    batch_size=64,
-    epochs=20,
-    validation_data=(
-        interact_test_df['encoded_contentId'].values,
-        interact_test_df['eventStrength'].values
-    ))
+train_inputs = {
+    'user': train_df['userIndex'].values,
+    'content': train_df['contentIndex'].values
+}
+
+test_inputs = {
+    'user': test_df['userIndex'].values,
+    'content': test_df['contentIndex'].values
+}
+
+model.fit(train_inputs,
+          train_df['eventStrength'].values,
+          validation_data=(test_inputs, test_df['eventStrength'].values),
+          epochs=5,
+          batch_size=32)
+
+loss = model.evaluate(test_inputs,
+                      test_df['eventStrength'].values)
+
+print(f'Test Loss: {loss}')
+
+def recommend_content_for_user(user_id, num_recommendations=5):
+    user_index = user_id_to_index.get(user_id, None)
+
+    if user_index is not None:
+        all_content_indices = np.arange(len(content_ids))
+
+        user_interactions = interact_df[interact_df['userIndex'] == user_index]['contentIndex'].values
+        candidate_content_indices = np.setdiff1d(all_content_indices, user_interactions)
+
+        user_inputs = {'user': np.full(len(candidate_content_indices), user_index), 'content': candidate_content_indices}
+        predicted_strengths = model.predict(user_inputs)
+
+        top_indices = np.argsort(predicted_strengths[:, 0])[::-1][:num_recommendations]
+
+        top_content_titles = [content_df.loc[content_df['contentId'] == content_ids[idx], 'title'].values[0]
+                              for idx in candidate_content_indices[top_indices]]
+
+        return top_content_titles
+
+    else:
+        return None
 
 
-test_loss = content_model.evaluate(
-    x=interact_test_df['encoded_contentId'].values,
-    y=interact_test_df['eventStrength'].values
-)
+list_usr = list(user_id_to_index.keys())
+example_user_id = list_usr[0]
+num_recommendations = 5
 
-print(f'Test Loss: {test_loss}')
+recommended_content = recommend_content_for_user(example_user_id, num_recommendations)
 
-
-def recommend_content_for_user(user_id, model, interact_full_df, content_encoder):
-    user_content = interact_full_df[interact_full_df['encoded_personId'] == user_id]['encoded_contentId'].unique()
-    all_content = np.arange(len(content_encoder.classes_))
-
-    new_content = np.setdiff1d(all_content, user_content)
-
-    predictions = model.predict(new_content)
-
-    predicted_content_ids = content_encoder.inverse_transform(new_content)
-
-    recommendations_df = pd.DataFrame({
-        'contentId': predicted_content_ids,
-        'predicted_eventStrength': predictions.flatten()
-    })
-
-    # Sort the recommendations by predicted eventStrength
-    recommendations_df = recommendations_df.sort_values(by='predicted_eventStrength', ascending=False)
-
-    return recommendations_df
-
-
-user_id_to_recommend = interact_test_df['encoded_personId'].values[0]
-recommendations = recommend_content_for_user(user_id_to_recommend, content_model, interact_full_df,
-                                             content_encoder)
-
-print('Recommendations for user:', person_encoder.inverse_transform([user_id_to_recommend])[0])
-print(recommendations.head(10))
+if recommended_content is not None:
+    print(f'Top {num_recommendations} recommended content for user {example_user_id}: {recommended_content}')
+else:
+    print(f'User ID not found in the mapping.')
 
