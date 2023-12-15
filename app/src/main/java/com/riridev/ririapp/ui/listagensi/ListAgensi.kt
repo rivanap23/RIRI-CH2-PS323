4faf44a3b185b53bf65ad5122fb72ca6912e3504
yowwahh <127203208+yowwahh@users.noreply.github.com>
2023-12-16 02:13:24 +0700

    Update ListAgensi.kt

--- app/src/main/java/com/riridev/ririapp/ui/listagensi/ListAgensi.kt
+++ app/src/main/java/com/riridev/ririapp/ui/listagensi/ListAgensi.kt
@@ -1,14 +1,26 @@
 package com.riridev.ririapp.ui.listagensi
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.riridev.ririapp.data.dummy.InstansiDummy
+import com.riridev.ririapp.data.dummy.AgensiDummy
 import com.riridev.ririapp.databinding.ActivityListAgensiBinding
 import com.riridev.ririapp.ui.adapter.ListAgensiAdapter
 
 class ListAgensi : AppCompatActivity() {
     private lateinit var binding: ActivityListAgensiBinding
+    private val adapter = ListAgensiAdapter { agensi ->
+        // Handle click event on agensi item
+        val bundle = Bundle()
+        bundle.putSerializable("AGENSI_DATA", agensi)
+        val intent = Intent(this, DetailAgensi::class.java)
+        intent.putExtras(bundle)
+        startActivity(intent)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityListAgensiBinding.inflate(layoutInflater)
@@ -17,11 +29,10 @@ class ListAgensi : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this)
         binding.rvAgensi.layoutManager = layoutManager
 
-        val adapter = ListAgensiAdapter {
-            //do nothing
-        }
-        adapter.submitList(InstansiDummy.dataInstansi)
+        // Set adapter for RecyclerView
         binding.rvAgensi.adapter = adapter
+
+        // Set data for adapter (replace with your actual data source)
+        adapter.submitList(AgensiDummy.dataAgensi)
     }
 }
-
