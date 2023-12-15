af249d0bcd4412f8f614d9049b944806f76a6db7
yowwahh <127203208+yowwahh@users.noreply.github.com>
2023-12-16 02:13:05 +0700

    Update DetailAgensi.kt

--- app/src/main/java/com/riridev/ririapp/ui/listagensi/DetailAgensi.kt
+++ app/src/main/java/com/riridev/ririapp/ui/listagensi/DetailAgensi.kt
@@ -4,36 +4,26 @@ package com.riridev.ririapp.ui.listagensi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import com.bumptech.glide.Glide
-import com.riridev.ririapp.R
 import com.riridev.ririapp.data.model.Agensi
-import de.hdodenhof.circleimageview.CircleImageView
+import com.riridev.ririapp.databinding.ActivityDetailAgensiBinding
+
 
 class DetailAgensi : AppCompatActivity() {
 
-    private lateinit var agensi: Agensi
+    private lateinit var binding: ActivityDetailAgensiBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_agensi)
+        binding = ActivityDetailAgensiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Dapatkan data agensi dari intent
-        agensi = intent.getParcelableExtra("agensi")!!
+        val agensi = intent.getSerializableExtra("AGENSI_DATA") as Agensi
 
-        // Set data agensi ke view
-        val imgBckground = findViewById<ImageView>(R.id.img_background)
-        Glide.with(this).load(agensi.bgImage).into(imgBckground)
-
-        val imgPhoto = findViewById<CircleImageView>(R.id.detail_img_item)
-        Glide.with(this).load(agensi.imageUrl).into(imgPhoto)
-
-        val tvName = findViewById<TextView>(R.id.detail_tv_name)
-        tvName.text = agensi.name
-
-        val tvDescription = findViewById<TextView>(R.id.detail_tv_description)
-        tvDescription.text = agensi.desc
+       Glide.with(this).load(agensi.bgImage).into(binding.imgBackground)
+        Glide.with(this).load(agensi.imageUrl).into(binding.detailImgItem)
+        binding.detailTvName.text = agensi.name
+        binding.tvItemDescription.text = agensi.desc
 
     }
 }
