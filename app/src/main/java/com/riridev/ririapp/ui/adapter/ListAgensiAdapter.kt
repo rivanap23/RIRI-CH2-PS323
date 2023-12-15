8858f4ff76d2fc4d9994503b502d0c4350adc4a8
yowwahh <127203208+yowwahh@users.noreply.github.com>
2023-12-15 21:31:55 +0700

    Add files via upload

---
+++ app/src/main/java/com/riridev/ririapp/ui/adapter/ListAgensiAdapter.kt
@@ -0,0 +1,62 @@
+package com.riridev.ririapp.ui.adapter
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.riridev.ririapp.data.model.Instansi
+import com.riridev.ririapp.databinding.ItemAgensiBinding
+
+
+class ListAgensiAdapter(
+    val onClick: (Instansi) -> Unit
+): ListAdapter<Instansi, ListAgensiAdapter.ListAgensiViewHolder>(DIFF_CALLBACK) {
+    class ListAgensiViewHolder(private val binding: ItemAgensiBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(instansi: Instansi) {
+            binding.nameInstansi.text = instansi.name
+            Glide.with(binding.root)
+                .load(instansi.imageUrl)
+                .into(binding.logoInstansi)
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListAgensiViewHolder {
+        val binding = ItemAgensiBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListAgensiViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListAgensiViewHolder, position: Int) {
+        val instansi = getItem(position)
+        holder.bind(instansi)
+        holder.itemView.setOnClickListener {
+            onClick(instansi)
+        }
+    }
+
+
+    companion object {
+        val DIFF_CALLBACK =
+            object : DiffUtil.ItemCallback<Instansi>() {
+                override fun areItemsTheSame(
+                    oldItem: Instansi,
+                    newItem: Instansi,
+                ): Boolean {
+                    return oldItem == newItem
+                }
+
+                override fun areContentsTheSame(
+                    oldItem: Instansi,
+                    newItem: Instansi,
+                ): Boolean {
+                    return oldItem == newItem
+                }
+            }
+    }
+}
