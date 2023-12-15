3480b723a9836e1b200d77f5d79d2608891494ba
yowwahh <127203208+yowwahh@users.noreply.github.com>
2023-12-16 02:13:48 +0700

    Update ListAgensiAdapter.kt

--- app/src/main/java/com/riridev/ririapp/ui/adapter/ListAgensiAdapter.kt
+++ app/src/main/java/com/riridev/ririapp/ui/adapter/ListAgensiAdapter.kt
@@ -7,19 +7,19 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.riridev.ririapp.data.model.Instansi
+import com.riridev.ririapp.data.model.Agensi
 import com.riridev.ririapp.databinding.ItemAgensiBinding
 
 
 class ListAgensiAdapter(
-    val onClick: (Instansi) -> Unit
-): ListAdapter<Instansi, ListAgensiAdapter.ListAgensiViewHolder>(DIFF_CALLBACK) {
+    val onClick: (Agensi) -> Unit
+): ListAdapter<Agensi, ListAgensiAdapter.ListAgensiViewHolder>(DIFF_CALLBACK) {
     class ListAgensiViewHolder(private val binding: ItemAgensiBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(instansi: Instansi) {
-            binding.nameInstansi.text = instansi.name
+        fun bind(agensi: Agensi) {
+            binding.nameInstansi.text = agensi.name
             Glide.with(binding.root)
-                .load(instansi.imageUrl)
+                .load(agensi.imageUrl)
                 .into(binding.logoInstansi)
         }
     }
@@ -33,27 +33,27 @@ class ListAgensiAdapter(
     }
 
     override fun onBindViewHolder(holder: ListAgensiViewHolder, position: Int) {
-        val instansi = getItem(position)
-        holder.bind(instansi)
+        val agensi = getItem(position)
+        holder.bind(agensi)
         holder.itemView.setOnClickListener {
-            onClick(instansi)
+            onClick(agensi)
         }
     }
 
 
     companion object {
         val DIFF_CALLBACK =
-            object : DiffUtil.ItemCallback<Instansi>() {
+            object : DiffUtil.ItemCallback<Agensi>() {
                 override fun areItemsTheSame(
-                    oldItem: Instansi,
-                    newItem: Instansi,
+                    oldItem: Agensi,
+                    newItem: Agensi,
                 ): Boolean {
                     return oldItem == newItem
                 }
 
                 override fun areContentsTheSame(
-                    oldItem: Instansi,
-                    newItem: Instansi,
+                    oldItem: Agensi,
+                    newItem: Agensi,
                 ): Boolean {
                     return oldItem == newItem
                 }
