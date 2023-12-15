77075a24bdbeaa3ad74f90b8b73f9e3cad109c67
yowwahh <127203208+yowwahh@users.noreply.github.com>
2023-12-16 02:14:34 +0700

    Update Agensi.kt

--- app/src/main/java/com/riridev/ririapp/data/model/Agensi.kt
+++ app/src/main/java/com/riridev/ririapp/data/model/Agensi.kt
@@ -1,12 +1,12 @@
 package com.riridev.ririapp.data.model
 
-
+import java.io.Serializable
 
 
 data class Agensi(
     val id: Int,
     val name: String,
-    val bgImage: String = "",
-    val imageUrl: String = "",
+    val bgImage: String ,
+    val imageUrl: String ,
     val desc: String
-)
+) : Serializable
