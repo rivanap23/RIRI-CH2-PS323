4eb36c32ef2ac8ebd873b73e8e7967dd875b54d7
Made Wahyu Adwitya Pramana <91488883+wahpram@users.noreply.github.com>
2023-12-16 23:19:17 +0800

    Update fake_detection.py

--- fake_detection.py
+++ fake_detection.py
@@ -1,69 +1,74 @@
-import math
+import random
 import numpy as np
-import scipy
 import pandas as pd
-import sklearn
-import stopwords
 import tensorflow as tf
-from typing import Dict, Text
-from sklearn.preprocessing import LabelEncoder
-from tensorflow import keras
+from matplotlib import pyplot as plt
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
-data = pd.read_csv('../data/dataset_fake_2.csv')
+data = pd.read_csv('../data/dataset_fake_3.csv')
 data = data.sample(frac=1)
-
-# sentences = []
-# labels = []
 
 sentences = data['Text']
 labels = data['Label']
 
-trainig_size = 360
-vocab_size = 10000
-max_length = 32
-embedding_dim = 16
+max_examples = 1995
+max_length = 16
+embedding_dim = 100
 trunc_type = 'post'
 padding_type = 'post'
 oov_token = '<OOV>'
+trainig_split = 0.9
 
-training_sentences = sentences[0:trainig_size]
-testing_sentences = sentences[trainig_size:]
+sentences_and_labels = list(zip(sentences, labels))
 
-training_labels = labels[0:trainig_size]
-testing_labels = labels[trainig_size:]
+random.seed(42)
+sentences_and_labels = random.sample(sentences_and_labels, max_examples)
 
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+sentences, labels = zip(*sentences_and_labels)
+print(f"There are {len(sentences)} sentences and {len(labels)} labels after random sampling\n")
 
+training_size = int(len(sentences)*trainig_split)
+
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+print(f"There are {len(training_sentences)} sentences for training.")
+print(f"There are {len(training_labels)} labels for training.")
+print(f"There are {len(testing_sentences)} sentences for validation.")
+print(f"There are {len(testing_labels)} labels for validation.")
+
+tokenizer = Tokenizer(num_words=len(training_sentences), oov_token="")
 tokenizer.fit_on_texts(training_sentences)
+
 word_index = tokenizer.word_index
+vocab_size = len(word_index)
 
 training_seq = tokenizer.texts_to_sequences(training_sentences)
 training_pad = pad_sequences(training_seq, maxlen=max_length, padding=padding_type, truncating=trunc_type)
-
 testing_seq = tokenizer.texts_to_sequences(testing_sentences)
 testing_pad = pad_sequences(testing_seq, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+print(f"Padded and truncated training sequences have shape: {training_pad.shape}")
+print(f"Padded and truncated validation sequences have shape: {testing_pad.shape}")
 
 training_labels = np.array(training_labels)
 testing_labels = np.array(testing_labels)
 
-gap1d_layer = tf.keras.layers.GlobalAveragePooling1D()
-
-sample_array = np.array([[[10, 2], [1, 3], [1, 1]]])
-
-print(f'sample of array = {sample_array.shape}')
-print(f'sample array: {sample_array}')
-
-output = gap1d_layer(sample_array)
-
-print(f'output shape of gap1d_layer: {output.shape}')
-print(f'output array of gap1d_layer: {output.numpy()}')
-
 model = tf.keras.models.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
-    tf.keras.layers.GlobalAveragePooling1D(),
-    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Conv1D(32, 5, activation='relu'),
+    tf.keras.layers.MaxPooling1D(pool_size=4),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.LSTM(32),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+    # Add L2 regularization
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
@@ -80,7 +85,7 @@ history = model.fit(training_pad,
                     callbacks=[],
                     verbose=2)
 
-new_sentence = ["alsdjfielakjsdifekljsadf"]
+new_sentence = ["Wahyu pramana berjalan di atas air"]
 
 new_seq = tokenizer.texts_to_sequences(new_sentence)
 new_pad = pad_sequences(new_seq, maxlen=max_length, padding=padding_type, truncating=trunc_type)
@@ -93,4 +98,16 @@ print(f'Predictions: {predictions}')
 binary_predictions = (predictions > 0.5).astype(int)
 print(f'Binary Predictions: {binary_predictions}')
 
+
+def plot_graphs(history, metric):
+    plt.plot(history.history[metric])
+    plt.plot(history.history[f'val_{metric}'])
+    plt.xlabel("Epochs")
+    plt.ylabel(metric)
+    plt.legend([metric, f'val_{metric}'])
+    plt.show()
+
+
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
 
