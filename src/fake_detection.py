33df45a0f457b583cbc7c9510b8592b9f95c9cf4
Made Wahyu Adwitya Pramana <91488883+wahpram@users.noreply.github.com>
2023-12-20 21:36:46 +0800

    Update fake_detection.py

--- src/fake_detection.py
+++ src/fake_detection.py
@@ -113,20 +113,3 @@ model.save('fake_detection_v2.h5')
 model = keras.models.load_model('model_h5/fake_detection_v2.h5')
 
 print('Model loaded!')
-
-# model_json = model.to_json()
-# with open("model.json", "w") as json_file:
-#     json_file.write(model_json)
-# # serialize weights to HDF5
-# model.save_weights("model.h5")
-# print("Saved model to disk")
-#
-#
-# json_file = open('model.json', 'r')
-# loaded_model_json = json_file.read()
-# json_file.close()
-# loaded_model = model_from_json(loaded_model_json)
-#
-# loaded_model.load_weights("model.h5")
-# print("Loaded model from disk")
-
